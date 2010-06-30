c829d050b297c2b9d471af3393bcd6b28bca6f21
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-06-30 17:35:36 +0100

    embedded bugs uno

---
+++ spec/integration/embedded_bugs_1_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+describe "Embedded Bugs Uno" do
+  context "#<<" do
+    before do
+      Person.delete_all
+      @person = Person.create
+      @person.addresses << Address.new(:street => "Old Street")
+      @person.addresses << Address.new(:street => "Hoxton Street")
+      @person.save
+    end
+
+    it "thinks it added multiple items to an embedded collection" do
+      @person.addresses.count.should eql 2
+    end
+
+    it "seems not to be the case as per the database" do
+      Person.last.addresses.count.should eql 2
+    end
+  end
+
+  context "#=" do
+    before do
+      Person.delete_all
+      @person = Person.create
+      addresses = [Address.new(:street => "Old Street")]
+      addresses << Address.new(:street => "Hoxton Street")
+      @person.addresses = addresses
+      @person.save
+    end
+
+    it "thinks it added multiple items to an embedded collection" do
+      @person.addresses.count.should eql 2
+    end
+
+    it "seems not to be the case as per the database" do
+      Person.last.addresses.count.should eql 2
+    end
+  end
+end
