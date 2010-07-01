3cc34ea2c9e0c4d6baeb65c65f674704a68031da
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-07-01 11:30:21 +0100

    Failing specs for _destroy not evaluating to false with nested embeds_manies

---
+++ spec/integration/nested_spec.rb
@@ -0,0 +1,56 @@
+require "spec_helper"
+
+describe "Nested Forms" do
+
+  before(:each) do
+    Person.delete_all
+    @person = Person.new
+    @person.favorites.build(:title => "Ice Cream")
+    @person.favorites.build(:title => "Jello")
+    @person.save
+  end
+
+  shared_examples_for "a document with a destroyed embeds many" do    
+    it "destroys when _destroy evaluates to true" do
+      @person.save
+      @person.reload.favorites.count.should eql 1
+      @person.favorites.first.title.should == "Jello"
+    end
+  end
+
+  context "_destroy => true" do
+    before(:each) do
+      @person.favorites_attributes = {
+      "0" => { "_destroy" => true },
+      "1" => { "_destroy" => false,
+               "title"    => "Jello" }
+      }
+    end
+
+    it_should_behave_like "a document with a destroyed embeds many"
+  end
+
+  context "_destroy => 1" do
+    before(:each) do
+      @person.favorites_attributes = {
+      "0" => { "_destroy" => 1 },
+      "1" => { "_destroy" => 0,
+               "title"    => "Jello" }
+      }
+    end
+
+    it_should_behave_like "a document with a destroyed embeds many"
+  end
+
+  context "_destroy => '1'" do
+    before(:each) do
+      @person.favorites_attributes = {
+      "0" => { "_destroy" => '1' },
+      "1" => { "_destroy" => '0',
+               "title"    => "Jello" }
+      }
+    end
+
+    it_should_behave_like "a document with a destroyed embeds many"
+  end
+end
